474d1be8fda63e172f106d9fa33a2acb1e2b9578
Zinedine Galouz <archwoudelakarai@gmail.com>
2022-02-03 12:13:37 +0100

    Remove lastUpdate file.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 const yamlReader = require("./yamlReader");
 const calendarHandler = require("./calendarHandler");
-const fs = require("fs");
 
-fs.writeFileSync("lastUpdate.txt", (new Date()).toLocaleString("fr-FR", {timeZone: "Europe/Paris"}));
 const sources = yamlReader.retrieveSources("Liste", "liste.yaml");
 calendarHandler.handleData(sources);
